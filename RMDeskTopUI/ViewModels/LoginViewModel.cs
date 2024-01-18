3ba0c3e6952f34f9a1376128835734b24d79a82e
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-01-17 22:56:17 -0300

    Login form

--- RMDeskTopUI/ViewModels/LoginViewModel.cs
+++ RMDeskTopUI/ViewModels/LoginViewModel.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Caliburn.Micro;
 
 namespace RMDeskTopUI.ViewModels
 {
-    public class ShellViewModel: Conductor<object>
+    public class LoginViewModel
     {
-
     }
 }
